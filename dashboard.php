74ea239405707b4b3e0fab59f061769cc7a78177
vasanthroriri <vasanth@roririsoft.com>
2024-07-02 19:36:05 +0530

    dashboard edit

--- dashboard.php
+++ dashboard.php
@@ -47,7 +47,7 @@
 
         <!-- Page Content -->
     <div id="content" class="container-fluid">
-        <div class="row">
+        <div class="row mt-5">
             <div class="col-lg-12">
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h1>Main Content Area</h1>
