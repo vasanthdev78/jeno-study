42bc625a09f156d322a83413410f65eb9c0a77e2
vasanthroriri <vasanthroriri@roririsoft.com>
2024-07-11 12:34:57 +0530

    expence complete

--- left.php
+++ left.php
@@ -108,6 +108,12 @@
     </a>
 </li>
 <li class="side-nav-item">
+    <a href="expence.php" class="side-nav-link">
+    <i class="bi bi-explicit"></i>
+        <span> Expense </span>
+    </a>
+</li>
+<li class="side-nav-item">
     <a data-bs-toggle="collapse" href="#sidebarBook" aria-expanded="false" aria-controls="sidebarEmail" class="side-nav-link">
         <i class="bi bi-person-vcard"></i>
         <span> Books </span>
