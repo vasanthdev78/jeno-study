3f87d0d05d9a6e161986d6973b9cf32e3602d6fc
vasanthroriri <vasanthroriri@roririsoft.com>
2024-07-16 17:30:10 +0530

    university edit

--- formUniversity.php
+++ formUniversity.php
@@ -54,24 +54,19 @@
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label for="universityName" class="form-label"><b>University Name</b><span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" placeholder="Enter University Name" name="universityName" id="universityName" required="required">
+                                    <input type="text" class="form-control" placeholder="Enter University Name" name="editUniversityName" id="editUniversityName" required="required">
                                 </div>
                             </div>
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label for="universityName" class="form-label"><b>Study Center Code</b><span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" pattern="^\S.*$" title="Please enter a value with no leading or trailing spaces" placeholder="Enter University Code" name="universityName" id="universityName" required="required">
+                                    <input type="text" class="form-control" pattern="^\S.*$" title="Please enter a value with no leading or trailing spaces" placeholder="Enter University Code" name="editStudyCode" id="editStudyCode" required="required">
                                 </div>
                             </div>
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <label for="universityImage" class="form-label"><b>University Image</b><span class="text-danger">*</span></label>
-                                    <input type="file" class="form-control" name="universityImage" id="universityImage">
-                                </div>
-                            </div>
+                            
                         </div><hr>
-                        <button type="button" id="addInputButton" class="btn btn-primary">Add Input</button>
-                        <div id="additionalInputs"></div>
+                        
+                        <div id="editItionalInputs"></div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
@@ -84,4 +79,57 @@
 
     <!-- --------------------------------------------------------------------------------------------------------------------------------------- -->
 
+
+    <div class="table-responsive d-none" id="universityView">
+          <form name="frm" method="post">
+              <input type="hidden" name="hdnAction" value="">
+            <div class="modal-header">
+              <h4 class="modal-title" id="myModalLabel">Task Details</h4>
+            </div>  
+           <div class="modal-body">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="card p-3">
+                                <h4>Name</h4> 
+                                <span class="detail"><?php echo $name;?></span>
+                            </div>
+                        </div>  
+                         <div class="col-sm-3 ">
+                            <div class="card p-3">
+                                <h4>Course Name</h4>
+                                <span class="detail"><?php echo  $course ;?></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card p-3">
+                                <h4>Place</h4>
+                                <span class="detail"><?php echo $location;?></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card p-3">
+                                <h4>Email Id</h4>
+                                <span class="detail"><?php echo $email;?></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card p-3">
+                                <h4>Mobile No.</h4>
+                                <span class="detail"><?php echo  $mobile;?></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 ">
+                            <div class="card p-3">
+                                <h4>Aadhar Number</h4>
+                                <span class="detail"><?php echo $aadhar;?></span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" onclick="javascript:location.href='student.php'" >Back</button>
+               
+            </div> 
+            </form>   
+      </div>
                      
